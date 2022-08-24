7e8446f02845ddd19f1c2b80a0b5b4b27e7a531b
Shelby Bowen <106043324+spbowen@users.noreply.github.com>
2022-08-23 20:24:44 -0400

    create app.js file and learn about Node

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello Node!');
+// Module 9 Practice Portfolio Challenge
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+//this is an example of using Arrow Functions.
+// Using an arrow function, we can avoid using the function keyword, parentheses around the function parameter, and the curly 
+//braces used to wrap a function, as we're only performing one action in the function. 
+//This is one of the benefits of arrow functions and array methods: we can keep simpler tasks on one line of code and make it more concise.
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
