3dee626a56e11be4c5b08dfc7370da3ecf806692
Shelby Bowen <106043324+spbowen@users.noreply.github.com>
2022-08-28 21:42:26 -0400

    generate webpage in app.js and add new folder for page template

---
+++ app.js
@@ -0,0 +1,35 @@
+/* Comment out for generate-webpage branch
+// Module 9 Practice Portfolio Challenge
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+//this is an example of using Arrow Functions.
+// Using an arrow function, we can avoid using the function keyword, parentheses around the function parameter, and the curly 
+//braces used to wrap a function, as we're only performing one action in the function. 
+//This is one of the benefits of arrow functions and array methods: we can keep simpler tasks on one line of code and make it more concise.
+
+
+
+ 
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+*/
+
+//This code below generatees an HTML - index file and places it in the foler with the given started code inside.
+
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require('./src/page-template.js');
